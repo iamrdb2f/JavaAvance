994fe9f09ce865e8eef23cc91f0fe697493854f0
Jean-Philippe Ehret <jehret@mohio.earth>
2021-07-30 13:33:56 +1200

    atomic

--- src/main/java/com/jnesis/jap/peartopear/connection/ConnectionCounter.java
+++ src/main/java/com/jnesis/jap/peartopear/connection/ConnectionCounter.java
@@ -3,19 +3,79 @@ package com.jnesis.jap.peartopear.connection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ConnectionCounter {
 
     private static final Logger LOG = LoggerFactory.getLogger(ConnectionCounter.class);
 
-    private int counter;
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    public List<String> messages=new ArrayList<>();
+
+    public int getValue() {
+        return counter.get();
+    }
 
     public void increment(){
-        counter++;
-        LOG.debug("Incremented new Value is "+counter);
+        //La synchronization d'une file de message ici est juste necessaire pour que l'affichage a la fin
+        //se fasse dans l'ordre, mais forcement cela ralentit u tout petit peu le traitement, apres votre test
+        //vous pouvez l'enlever
+        synchronized (messages) {
+            int newValue = counter.incrementAndGet();
+            messages.add("increment new Value is " + newValue);
+        }
     }
 
     public void decrement(){
-        counter--;
-        LOG.debug("Decremented new Value is "+counter);
+        //La synchronization d'une file de message ici est juste necessaire pour que l'affichage a la fin
+        //se fasse dans l'ordre, mais forcement cela ralentit u tout petit peu le traitement, apres votre test
+        //vous pouvez l'enlever
+        synchronized (messages) {
+            int newValue = counter.decrementAndGet();
+            messages.add("decrement new Value is " + newValue);
+        }
     }
+
+    /*
+
+    With compareAndSet instead of incrementAndGet / incrementAndSet
+
+    public void increment() {
+        while(true) {
+            int existingValue = getValue();
+            int newValue = existingValue + 1;
+            if (update(existingValue,newValue,"increment")){
+                break;
+            }
+        }
+    }
+
+    public void decrement() {
+        while(true) {
+            int existingValue = getValue();
+            int newValue = existingValue - 1;
+            if (update(existingValue,newValue,"decrement")){
+                break;
+            }
+        }
+    }
+
+    public boolean update(int existingValue, int newValue, String action){
+        //La synchronization d'une file de message ici est juste necessaire pour que l'affichage a la fin
+        //se fasse dans l'ordre, mais forcement cela ralentit u tout petit peu le traitement, apres votre test
+        //vous pouvez l'enlever
+        synchronized (messages) {
+            if (counter.compareAndSet(existingValue, newValue)) {
+                messages.add(action+" new Value is " + newValue);
+                return true;
+            }
+            else{
+                messages.add(action+" attempt SKIPPED " + newValue);
+                return false;
+            }
+        }
+    }*/
 }
--- src/test/java/com/jnesis/jap/peartopear/connection/ConnectionCounterTest.java
+++ src/test/java/com/jnesis/jap/peartopear/connection/ConnectionCounterTest.java
@@ -1,13 +1,29 @@
 package com.jnesis.jap.peartopear.connection;
 
+import com.jnesis.jap.peartopear.utils.Benchmark;
+
 public class ConnectionCounterTest {
 
     public static void main(String... args){
         ConnectionCounter counter=new ConnectionCounter();
         DecrementerThread thd=new DecrementerThread(counter);
         IncrementerThread thi=new IncrementerThread(counter);
+        Benchmark b=new Benchmark();
+        b.start();
         thd.start();
         thi.start();
+        try {
+            thd.join();
+            thi.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        b.stop();
+        System.out.println(b);
+        for (String message: counter.messages){
+            System.out.println(message);
+        }
     }
 
 }
